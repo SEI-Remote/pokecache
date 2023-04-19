8cbb05fc1fee49bc56f10ea8d7d561c042bc3737
Ben Manley <benjamintmanley@gmail.com>
2023-04-19 01:05:16 -0500

    add stylinglinks to all views, update landing page with conditional rendering

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import * as profilesCtrl from '../controllers/profiles.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
 router.get('/', profilesCtrl.index)
-router.get('/:profileId', profilesCtrl.show)
+router.get('/:profileId', isLoggedIn, profilesCtrl.show)
 
 export { router }
