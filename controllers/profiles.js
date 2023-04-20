2f1de3f1a412500f5cf6b68a26fdfbe0a6b2c352
Ben Manley <benjamintmanley@gmail.com>
2023-04-19 19:53:58 -0500

    add conditional rendering for all the buttons

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -3,8 +3,13 @@ import { Profile } from "../models/profile.js"
 function index(req, res) {
   Profile.find({})
   .then(profiles => {
-    res.render('profiles/index', {
-      profiles
+    Profile.findById(req.user.profile)
+    .then(userProfile => {
+      console.log(userProfile)
+      res.render('profiles/index', {
+        profiles,
+        userProfile
+      })
     })
   })
 }
@@ -18,7 +23,40 @@ function show(req, res) {
   })
 }
 
+function requestFriend(req, res) {
+  // find the logged in user's profile _id
+  Profile.findById(req.user.profile)
+  .then(userProfile => {
+    // push the friend _id into the pendingOutgoing
+    userProfile.pendingOutgoingInvites.push(req.params.profileId)
+    userProfile.save()
+    .then(() => {
+      // find the friend profile
+      Profile.findById(req.params.profileId)
+      .then(friendProfile => {
+        // push the logged in user's profile _id into pendingIncoming
+        friendProfile.pendingIncomingInvites.push(req.user.profile)
+        friendProfile.save()
+        .then(() => {
+          res.redirect('/profiles')
+        })
+      })
+    })
+  })
+}
+
+function acceptFriend(req, res) {
+
+}
+
+function removeFriend(req, res) {
+
+}
+
 export {
   index,
-  show
+  show,
+  requestFriend,
+  acceptFriend,
+  removeFriend
 }
--- routes/profiles.js
+++ routes/profiles.js
@@ -4,7 +4,11 @@ import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
-router.get('/', profilesCtrl.index)
+router.get('/', isLoggedIn, profilesCtrl.index)
 router.get('/:profileId', isLoggedIn, profilesCtrl.show)
+router.post('/requestFriend/:profileId', isLoggedIn, profilesCtrl.requestFriend)
+router.patch('/acceptFriend/:profileId', isLoggedIn, profilesCtrl.acceptFriend)
+router.delete('/removeFriend/:profileId', isLoggedIn, profilesCtrl.removeFriend)
+
 
 export { router }
